e5ab6604c72c60c45455f3e66a6ea31809ae725d
Angel <ANGEL.MARY@kalvium.community>
2025-04-03 10:54:42 +0530

    completed the assignment

--- script.js
+++ script.js
@@ -15,9 +15,14 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
-  // 2. If the cookie exists, increment the value and update the cookie
-  // 3. If the cookie does not exist, create it and set the value to 1
-  // 4. Display the count on the webpage
+  let count = parseInt(getCookie('count')) || 0;
 
-  // your code here
+  // 2. If the cookie exists, increment the value and update the cookie
+  count++;
+
+  // 3. If the cookie does not exist, create it and set the value to 1
+  setCookie('count',count,365);
+
+  // 4. Display the count on the webpage
+  document.body.innerHTML += `<h1>Page Visit Count: ${count}</h1>`;
 });
